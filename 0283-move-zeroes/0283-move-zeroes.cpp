6d5fe3db35e196292e9aaf525c937dd6f93f701e
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-06-24 07:39:46 +0530

    Time: 11 ms (90.63%), Space: 21.6 MB (73.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>temp;
+        int j=-1;
         int n=nums.size();
         for(int i=0; i<n; i++)
         {
+            if(nums[i]==0)
+            {
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;
+        
+        for(int i=j+1; i<n; i++)
+        {
             if(nums[i]!=0)
-                temp.push_back(nums[i]);
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-        int nx=temp.size();
-        for(int i=0; i<nx; i++)
-        {
-            nums[i]=temp[i];
-        }
-        for(int i=nx; i<n; i++)
-        {
-           nums[i]=0;
-        }
+        
         
     }
 };
-// time complexity:- O(2N)
+// time complexity:- O(N)
 //space complexity:- O(N)
