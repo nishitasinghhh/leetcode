5dfe1f48b3fd0b9271e2bed89844ffafb63f737a
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-15 17:07:44 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (31.57%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findSum(vector<int> &arr)
+    {
+        int n=arr.size();
+        int prev=arr[0];
+        int prev2=0;
+        for(int i=1; i<n; i++)
+        {
+            int pick=arr[i];
+            if(i>1) pick+=prev2;
+            int not_pick=0+prev;
+            int curi=max(pick,not_pick);
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        vector<int>temp1;
+        vector<int>temp2;
+        int n=nums.size();
+        if(n==1) return nums[0];
+        for(int i=0; i<n; i++)
+        {
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+        return max(findSum(temp1),findSum(temp2));
+    }
+};
