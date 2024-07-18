da7aa1769a6ced16331e09aa322dba258c332c3a
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-18 23:03:46 +0530

    Time: 52 ms (23.57%), Space: 47.1 MB (32.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int findMin(int index, int T,vector<int>& coins,     vector<vector<int>> &dp){
-        if(index==0)
-        {
-            if(T%coins[index]==0)
-                return T/coins[index];
-            else return 1e9;
-        }
-        if(dp[index][T]!=-1) return dp[index][T];
-        int notTake=0+findMin(index-1,T,coins, dp);
-        int take=1e9;
-        if(coins[index]<=T)
-        {
-            take=1+findMin(index,T-coins[index],coins,dp);
-        }
-        return dp[index][T]=min(take,notTake);
-    }
+   
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=findMin(n-1,amount,coins,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+       for(int i=0; i<=amount; i++)
+       {
+           if(i%coins[0]==0) dp[0][i]=i/coins[0];
+           else dp[0][i]=1e9;
+       }
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<=amount; j++)
+            {
+                int notTake=0+dp[i-1][j];
+                int take=1e9;
+                if(coins[i]<=j)
+                    take=1+dp[i][j-coins[i]];
+                    dp[i][j]=min(take,notTake);
+            }
+        }
+        int ans=dp[n-1][amount];
         if(ans>=1e9) return -1;
         return ans;
     }
