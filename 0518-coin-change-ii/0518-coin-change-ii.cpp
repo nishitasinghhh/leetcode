a35e2c218103e2bdd77d5d3fd397adbcb6a31b49
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-19 13:05:22 +0530

    Time: 10 ms (58.33%), Space: 20.2 MB (24.58%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findCoins(int index, int target, vector<int>& coins,
+              vector<vector<int>> &dp)
+    {
+        if(index==0)
+        {
+            return (target%coins[index]==0);
+        }
+        if(dp[index][target]!=-1) return dp[index][target];
+        int notTake=findCoins(index-1, target, coins, dp);
+        int take=0;
+        if(coins[index]<=target)
+        {
+            take=findCoins(index,target-coins[index],coins,dp);
+        }
+        return dp[index][target]=take+notTake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return findCoins(n-1, amount, coins, dp);
+    }
+};
