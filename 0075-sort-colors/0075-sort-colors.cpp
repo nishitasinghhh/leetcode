159d4958d473dde3feb19b949570cc523ed044d5
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-24 21:34:03 +0530

    Time: 2 ms (43.99%), Space: 10.1 MB (10.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        int count0=0;
-        int count1=0;
-        int count2=0;
-        for(int i=0; i<n; i++) // used to store the number of 0's 1'sand 2's int the 
-                               // given array
+       int n=nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
         {
-            if(nums[i]==0)
+            if(nums[mid]==0)
             {
-                count0++;
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            else if(nums[i]==1)
+            else if(nums[mid]==1)
             {
-                count1++;
+                mid++;
             }
             else
             {
-                count2++;
+                swap(nums[mid],nums[high]);
+                high--;
             }
-        }
-        for(int i=0; i<count0; i++) // updating 0;
-        {
-            nums[i]=0;
-        }
-        for(int i=count0; i<count0+count1; i++) // updating 1;
-        {
-            nums[i]=1;
-        }
-        for(int i=count0+count1; i<n; i++) // updating 2;
-        {
-            nums[i]=2;
         }
     }
 };
-// time complexity:- O(2N)
+// time complexity:- O(N)
 // space complexity:-o(1)
