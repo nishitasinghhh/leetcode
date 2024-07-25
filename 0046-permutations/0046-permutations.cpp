2d0dbdca32f73fe2dec4bc6007f35ae684cb97a9
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-25 21:28:34 +0530

    Time: 4 ms (42.87%), Space: 9.2 MB (49.70%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-void findPermute(vector<vector<int>>&ans, vector<int>ds, vector<int>nums,int n,int freq[])
-{
-    if(ds.size()==nums.size()) // base case where the size of permutation becomes equal to the number of element
+
+    void recurPermute(vector<int> &ds, vector<vector<int>> & ans, vector<int> &nums, int freq[])
+    {
+        if(ds.size()==nums.size())
         {
             ans.push_back(ds);
             return;
         }
-        for(int i=0; i<n; i++)
+        for(int i=0; i<nums.size(); i++)
         {
             if(!freq[i])
             {
                 ds.push_back(nums[i]);
                 freq[i]=1;
-                findPermute(ans,ds,nums,n,freq);
-            freq[i]=0;
-            ds.pop_back();
-            }
-            
-        }
-}
-    
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans; // store all the permutations
-        vector<int>ds; //single permutation
-        
-        int n=nums.size();
-        int freq[n]; // map
-            for(int i=0; i<n; i++)
-            {
+                recurPermute(ds,ans,nums,freq);
                 freq[i]=0;
+                ds.pop_back();
             }
-        findPermute(ans,ds,nums,n,freq);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        int freq[nums.size()];
+        for(int i=0; i<nums.size(); i++)
+        {
+            freq[i]=0;
+        }
+        recurPermute(ds,ans,nums,freq);
         return ans;
     }
 };
