aa12865ce9b3e958dc2f69f3457981edf0a5b168
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-07-27 18:32:44 +0530

    Time: 19 ms (85.54%), Space: 23 MB (36.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,27 +6,17 @@ public:
         vector<vector<int>>ans;
         for(int i=0; i<n; i++)
         {
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1])
+            if(ans.empty() || intervals[i][0]>ans.back()[1])
             {
-                continue;
+                ans.push_back(intervals[i]);
             }
-            for(int j=i+1; j<n; j++)
+            else
             {
-                if(intervals[j][0]<=end)
-                {
-                    end=max(end,intervals[j][1]);
-                }
-                else
-                {
-                    break;
-                }
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
     }
 };
-//time complexity:-O(NlogN)+O(2N);
+//time complexity:-O(NlogN)+O(N);
 //space complexity:- O(N)
