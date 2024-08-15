55986bff347dfed539c2a2f41ed3bfaa38aa35e6
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-16 01:53:57 +0530

    Time: 79 ms (89.83%), Space: 23.9 MB (41.80%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
+        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        dist[0][0]=0;
+        pq.push({0,{0,0}});
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            pq.pop();
+            int diff=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
+            if(r==n-1 && c==m-1) return diff;
+            int drow[]={-1,0,1,0};
+            int dcol[]={0,1,0,-1};
+            for(int i=0; i<4; i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m)
+                {
+                    int newEffort=max(abs(heights[nrow][ncol]-heights[r][c]),diff);
+                    if(newEffort<dist[nrow][ncol])
+                    {
+                        dist[nrow][ncol]=newEffort;
+                        pq.push({newEffort,{nrow,ncol}});
+                    }
+                }
+            }
+            
+        }
+        return 0;
+    }
+};
