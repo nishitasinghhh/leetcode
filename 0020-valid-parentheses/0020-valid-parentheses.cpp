1d0fb8f2627755007917464db5937dbf618cd870
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-18 22:47:44 +0530

    Time: 3 ms (40.89%), Space: 7.8 MB (45.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,15 +5,18 @@ public:
         for(int i=0; i<s.size(); i++)
         {
             if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            {
                 st.push(s[i]);
-            else 
+            }
+            else
             {
                 if(st.empty()) return false;
-                char c=st.top();
-                if((s[i]==')' &&c!='(') || (s[i]=='}' && c!='{') || (s[i]==']' && c!='['))
-                {
-                    return false;
-                }
+               
+                    char ch=st.top();
+                    if((s[i]==')' && ch!='(')||(s[i]=='}' && ch!='{')||(s[i]==']' && ch!='['))
+                    {
+                        return false;
+                    }
                 st.pop();
             }
         }
