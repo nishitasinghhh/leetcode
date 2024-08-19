c749f75780654883847dc0e901ba2a489abf91e3
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-20 03:05:02 +0530

    Time: 20 ms (37.97%), Space: 27 MB (44.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        int n=nums.size();
+      int n=nums.size();
+        int count=0;
+        int el;
         for(int i=0; i<n; i++)
         {
-            mpp[nums[i]]++;
-        }
-        int t=n/2;
-        for(auto it:mpp)
-        {
-            if(it.second >t)
+            if(count==0)
             {
-                return it.first;
+                count=1;
+                el=nums[i];
             }
+            else if(nums[i]==el) count++;
+            else count--;
         }
+        int cnt=0;
+        int m=n/2;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==el)
+                cnt++;
+        }
+        if(cnt>=m)
+            
+            return el;
         return -1;
     }
 };
-//time complexity:- O(NlogN)+O(N)
-//space complexity:-O(N)
+//Moore's Voting algorithm
+//time complexity:- O(N)+O(N)
+//space complexity:-O(1)
