cde024850e730f985c0838f17bf71c41cb8d12e3
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-20 15:30:49 +0530

    Time: 114 ms (66.99%), Space: 74.7 MB (28.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,22 +4,24 @@ public:
         int n=nums.size();
         if(n==0) return 0;
         int longest=1;
-        int count=0;
-        int lastSmaller=INT_MIN;
-        sort(nums.begin(),nums.end());
+        unordered_set<int>st;
         for(int i=0; i<n; i++)
         {
-            if(nums[i]-1==lastSmaller)
+            st.insert(nums[i]);
+        }
+        for(auto it: st)
+        {
+            if(st.find(it-1)==st.end())
             {
-                count++;
-                lastSmaller=nums[i];
+                int cnt=1;
+                int x=it;
+                while(st.find(x+1)!=st.end())
+                {
+                    x=x+1;
+                    cnt=cnt+1;
+                }
+                longest=max(longest,cnt);
             }
-            else if(nums[i]!=lastSmaller)
-            {
-                count=1;
-                lastSmaller=nums[i];
-            }
-            longest=max(longest,count);
         }
         return longest;
     }
