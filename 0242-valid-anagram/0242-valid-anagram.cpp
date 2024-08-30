901037d160e14342eca98f612ff1badc24f3f5d5
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-08-31 03:58:48 +0530

    Time: 7 ms (76.08%), Space: 8.8 MB (76.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
-            return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
+      const int chart=256;
+        if(s.size()!=t.size()) return false;
+        int count[chart]={0};
+        for(int i=0; i<s.size(); i++)
+        {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for(int i=0; i<256; i++)
+        {
+            if(count[i]!=0)
+                return false;
+        }
+        return true;
     }
+    
 };
