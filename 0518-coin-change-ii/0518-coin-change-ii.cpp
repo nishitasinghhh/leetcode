eb834ebbf3a5aee8300693a598338875d2b3c8d1
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-09-16 16:51:29 +0530

    Time: 3 ms (91.48%), Space: 20.1 MB (48.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int findCoins(int index, int target, vector<int>& coins,
-              vector<vector<int>> &dp)
-    {
-        if(index==0)
-        {
-            return (target%coins[index]==0);
-        }
-        if(dp[index][target]!=-1) return dp[index][target];
-        int notTake=findCoins(index-1, target, coins, dp);
-        int take=0;
-        if(coins[index]<=target)
-        {
-            take=findCoins(index,target-coins[index],coins,dp);
-        }
-        return dp[index][target]=take+notTake;
-    }
+   int findCoins(int index, int target, vector<int>& coins,vector<vector<int>> &dp)
+   {
+       if(index==0)
+       {
+           return (target%coins[index]==0);
+       }
+       if(dp[index][target]!=-1) return dp[index][target];
+       int not_take=findCoins(index-1, target,coins,dp);
+       int take=0;
+       if(coins[index]<=target)
+       {
+           take=findCoins(index,target-coins[index],coins,dp);
+       }
+       return dp[index][target]=take+not_take;
+   }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
+       int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return findCoins(n-1, amount, coins, dp);
+        return findCoins(n-1,amount,coins,dp);
     }
 };
