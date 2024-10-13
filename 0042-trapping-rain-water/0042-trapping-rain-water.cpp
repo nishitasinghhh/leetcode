352282904a083e203602932910365022e1cfd678
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-10-14 04:04:05 +0530

    Time: 13 ms (54.46%), Space: 26.8 MB (5.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    vector<int> findPrefixMax(vector<int> &heights)
+    
+   vector<int> findPrefixMax(vector<int> heights,int n)
+   {
+     
+       vector<int>prefix_max(n);
+       prefix_max[0]=heights[0];
+       for(int i=1; i<n; i++)
+       {
+           prefix_max[i]=max(prefix_max[i-1],heights[i]);
+       }
+       return prefix_max;
+       
+   }
+    vector<int> findSuffixMax(vector<int> heights,int n)
     {
-        int n=heights.size();
-        vector<int>prefix(n);
-        prefix[0]=heights[0];
-        for(int i=1; i<heights.size(); i++)
-        {
-            prefix[i]=max(prefix[i-1],heights[i]);
-        }
-        return prefix;
-    }
-    vector<int> findSuffixMax(vector<int> &heights)
-    {
-        int n=heights.size();
-        vector<int>suffix(n);
-        suffix[n-1]=heights[n-1];
+        vector<int>suffix_max(n);
+        suffix_max[n-1]=heights[n-1];
         for(int i=n-2; i>=0; i--)
         {
-            suffix[i]=max(suffix[i+1],heights[i]);
+            suffix_max[i]=max(suffix_max[i+1],heights[i]);
         }
-        return suffix;
+        return suffix_max;
     }
     int trap(vector<int>& heights) {
-        int total=0;
-        vector<int>prefixMax=findPrefixMax(heights);
-            vector<int>suffixMax=findSuffixMax(heights);
-        for(int i=0; i<heights.size(); i++)
+      int total=0;
+        int n=heights.size();
+        vector<int>prefixMax=findPrefixMax(heights,n);
+        vector<int>suffixMax=findSuffixMax(heights,n);
+        for(int i=0; i<n; i++)
         {
-            
             int leftMax=prefixMax[i];
             int rightMax=suffixMax[i];
             if(heights[i]<leftMax && heights[i]<rightMax)
