d2f665a87c41d94b94e7febf1cc1ce9f769b9827
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-10-18 01:40:39 +0530

    Time: 27 ms (99.96%), Space: 38.1 MB (44.20%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& mat,vector<vector<int>>& vis,vector<vector<int>>& dis)
+    void bfs(vector<vector<int>>& mat,vector<vector<int>>& vis,vector<vector<int>>& dist)
     {
         int n=mat.size();
         int m=mat[0].size();
-       queue<pair<pair<int,int>,int>>q;
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
@@ -18,31 +18,32 @@ public:
         }
         while(!q.empty())
         {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int steps=q.front().second;
+            auto it=q.front();
             q.pop();
-            dis[r][c]=steps;
+            int row=it.first.first;
+            int col=it.first.second;
+            int step=it.second;
+            dist[row][col]=step;
             int drow[]={-1,0,1,0};
             int dcol[]={0,1,0,-1};
             for(int i=0; i<4; i++)
             {
-                int row=r+drow[i];
-                int col=c+dcol[i];
-                if(row>=0 && row<n && col>=0 && col<m && vis[row][col]==0)
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] )
                 {
-                    q.push({{row,col},steps+1});
-                    vis[row][col]=1;
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},step+1});
                 }
             }
         }
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-       int n=mat.size();
+      int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>dis(n,vector<int>(m,0));
-        bfs(mat,vis,dis);
-        return dis;
+        vector<vector<int>>dist(n,vector<int>(m,0));
+        bfs(mat,vis,dist);
+        return dist;
     }
 };
