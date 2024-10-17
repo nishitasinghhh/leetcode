96a07b4b89fd87a4d7f436ab44c8b500f8fd9689
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-10-17 09:02:03 +0530

    Time: 3 ms (84.92%), Space: 16.5 MB (39.25%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-   void bfs(vector<vector<int>>& grid,int &tm,vector<vector<int>>&vis)
+   void bfs(vector<vector<int>>& grid, vector<vector<int>> &vis,int &tm)
    {
-       int n=grid.size();
-       int m=grid[0].size();
        queue<pair<pair<int,int>,int>>q;
-      for(int i=0; i<n; i++)
-      {
-          for(int j=0; j<m; j++)
-          {
-              if(grid[i][j]==2)
-              {
-                  q.push({{i,j},0});
-                  vis[i][j]=2;
-              }
-          }
-      }
+       int n=grid.size();
+        int m=grid[0].size();
+       for(int i=0; i<n; i++)
+       {
+           for(int j=0; j<m; j++)
+           {
+               if(grid[i][j]==2)
+               {
+                   q.push({{i,j},0});
+                   vis[i][j]=2;
+               }
+           }
+       }
+       int drow[]={-1,0,1,0};
+       int dcol[]={0,1,0,-1};
        while(!q.empty())
        {
            int r=q.front().first.first;
            int c=q.front().first.second;
            int t=q.front().second;
            q.pop();
-           int drow[]={-1,0,1,0};
-           int dcol[]={0,1,0,-1};
            tm=max(tm,t);
            for(int i=0; i<4; i++)
            {
                int nrow=r+drow[i];
                int ncol=c+dcol[i];
-               if(nrow>=0 && nrow<n  && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1)
+               if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1)
                {
                    q.push({{nrow,ncol},t+1});
-                   vis[nrow][ncol]=2;
+                       vis[nrow][ncol]=2;
                }
            }
        }
@@ -40,15 +40,17 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int tm=0;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        bfs(grid,tm,vis);
+        int tm=0;
+        bfs(grid,vis,tm);
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
                 if(vis[i][j]==0 && grid[i][j]==1)
+                {
                     return -1;
+                }
             }
         }
         return tm;
