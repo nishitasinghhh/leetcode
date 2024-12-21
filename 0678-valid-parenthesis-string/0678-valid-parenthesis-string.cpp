c0523cf5496bd4cb6dd4aa5cec58ce55a9e275b7
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2024-12-21 21:08:53 +0530

    Time: 0 ms (100.00%), Space: 8 MB (39.28%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -3,29 +3,30 @@ public:
     
     bool checkValidString(string s) {
         int n=s.size();
-        int min=0;
-        int max=0;
+      int mini=0;
+        int maxi=0;
         for(int i=0; i<n; i++)
         {
             if(s[i]=='(')
             {
-                min+=1;
-                max+=1;
+                mini=mini+1;
+                maxi=maxi+1;
             }
             else if(s[i]==')')
             {
-                min+=-1;
-                max+=-1;
+                mini=mini-1;
+                maxi=maxi-1;
             }
             else
             {
-                min=min-1;
-                max=max+1;
+                mini=mini-1;
+                maxi=maxi+1;
             }
-            if(min<0) min=0;
-            if(max<0) return false;
+            if(mini<0) mini=0;
+            if(maxi<0) return false;
         }
-        return (min==0);
+        return (mini==0);
+        
     }
 };
 //time complexity:-(N)
