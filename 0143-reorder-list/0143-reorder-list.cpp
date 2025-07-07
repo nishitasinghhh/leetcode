59186883a8569c6c41f6f5ca17d46c2da2b4f8c6
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-07-07 10:16:28 +0530

    Time: 0 ms (100%), Space: 22.7 MB (80.05%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head)
+        return;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* second=slow->next;
+        slow->next=nullptr;
+        ListNode* node=nullptr;
+        while(second)
+        {
+            ListNode* front=second->next;
+            second->next=node;
+            node=second;
+            second=front;
+        }
+        ListNode* first=head;
+        second=node;
+        while(second)
+        {
+            ListNode* temp1=first->next;
+            ListNode* temp2=second->next;
+            first->next=second;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
+        }
+    }
+};
