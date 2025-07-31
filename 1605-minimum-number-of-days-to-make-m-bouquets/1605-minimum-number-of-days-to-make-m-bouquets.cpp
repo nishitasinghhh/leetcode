9238257af74f409751f76a7165f6e2f11ee079b6
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-07-31 10:46:54 +0530

    Time: 20 ms (72.33%), Space: 70.4 MB (97.56%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool Possible(vector<int>& bloomDay,int m,int k,int mid)
+    {
+        int cnt=0;
+        int no_of_b=0;
+        for(int i=0; i<bloomDay.size(); i++)
+        {
+            if(bloomDay[i]<=mid)
+            {
+                cnt++;
+            }
+            else
+            {
+                no_of_b+=(cnt/k);
+                cnt=0;
+            }
+        }
+         no_of_b+=(cnt/k);
+        if(no_of_b>=m)
+            return true;
+        return false;
+    }
+    int findMax(vector<int>& bloomDay)
+    {
+        int maxi=INT_MIN;
+        for(int i=0; i<bloomDay.size(); i++)
+        {
+              maxi=max(maxi,bloomDay[i]);
+        }
+      return maxi;
+    }
+int findMin(vector<int>& bloomDay)
+{
+    int mini=INT_MAX;
+    for(int i=0; i<bloomDay.size(); i++)
+    {
+        mini=min(mini,bloomDay[i]);
+    }
+    return mini;
+}
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long val=m*1ll*k*1ll;
+        int n=bloomDay.size();
+        if(val>n) return -1;
+        int low=findMin(bloomDay);
+        int high=findMax(bloomDay);
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(Possible(bloomDay,m,k,mid))
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
