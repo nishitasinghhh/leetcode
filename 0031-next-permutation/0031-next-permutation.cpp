e600ac3bfd49fc59b192833dafa0158e981ff240
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-07-31 21:21:29 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-      int n=nums.size();
-        int ind=-1;
+        int n=nums.size();
+        int index=-1;
         for(int i=n-2; i>=0; i--)
         {
-            if(nums[i]<nums[i+1])
+            if(nums[i+1]>nums[i])
             {
-                ind=i;
-                break;
+index=i;
+            break;
             }
+            
         }
-        if(ind==-1)
+        if(index==-1)
         {
             reverse(nums.begin(),nums.end());
             return;
         }
-        for(int i=n-1; i>=ind; i--)
+        for(int i=n-1; i>=index; i--)
         {
-            if(nums[ind]<nums[i])
+            if(nums[i]>nums[index])
             {
-                swap(nums[ind],nums[i]);
+                swap(nums[i],nums[index]);
                 break;
             }
         }
-        reverse(nums.begin()+ind+1,nums.end());
+        reverse(nums.begin()+index+1,nums.end());
         return;
-        
     }
 };
-// time complexity:-O(N)+O(N)+O(N);
-//space complexity:-O(N)
