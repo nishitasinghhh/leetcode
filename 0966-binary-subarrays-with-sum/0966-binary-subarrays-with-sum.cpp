29277d340754edfd2f1f12315f6493ff54e277b6
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-08-01 18:07:36 +0530

    Time: 0 ms (100%), Space: 32.5 MB (97.38%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int find(vector<int>& nums, int goal)
+{
+    if(goal<0)
+    return 0;
+    int l=0,r=0,sum=0,count=0;
+    while(r<nums.size())
+    {
+        sum+=nums[r];
+        while(sum>goal)
+        {
+            sum-=nums[l];
+            l++;
+        }
+        count+=(r-l+1);
+        r++;
+    }
+    return count;
+}
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int ans=find(nums,goal)-find(nums,goal-1);
+        return ans;
+    }
+};
