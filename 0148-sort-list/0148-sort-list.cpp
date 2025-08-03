5bf8a86a1d28826bbf11afc6426eb936493b086f
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-08-04 04:43:14 +0530

    Time: 39 ms (52.44%), Space: 75.8 MB (21.71%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,6 +1,6 @@
 /**
  * Definition for singly-linked list.
- * /struct ListNode {
+ * struct ListNode {
  *     int val;
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
@@ -10,65 +10,57 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* leftHead,ListNode* rightHead)
+ListNode* merge(ListNode* list1, ListNode* list2)
+{
+    ListNode* dummyNode=new ListNode(-1);
+    ListNode* temp=dummyNode;
+    while(list1!=NULL && list2!=NULL)
     {
-        if(leftHead==NULL) return rightHead;
-        if(rightHead==NULL) return leftHead;
-        ListNode* t1=leftHead;
-            ListNode* t2=rightHead;
-        ListNode* dummy=new ListNode(-1);
-        ListNode* temp=dummy;
-        while(t1!=NULL && t2!=NULL)
+        if(list1->val<=list2->val)
         {
-            if(t1->val<=t2->val)
-            {
-                temp->next=t1;
-                temp=t1;
-                t1=t1->next;
-            }
-            else
-            {
-          temp->next=t2;
-                temp=t2;
-                t2=t2->next;
-            }
+            temp->next=list1;
+            list1=list1->next;
         }
-        while(t1)
+        else
         {
-            temp->next=t1;
-                temp=t1;
-                t1=t1->next;
+            temp->next=list2;
+            list2=list2->next;
         }
-        while(t2)
-        {
-             temp->next=t2;
-                temp=t2;
-                t2=t2->next;
-        }
-        return dummy->next;
+        temp=temp->next;
     }
-    ListNode* findMiddle(ListNode* head)
-    { if (head == NULL || head->next == NULL) {
-            return head;
-        }
-        ListNode* slow=head;
-        ListNode* fast=head->next;
-        while(fast!=NULL && fast->next!=NULL)
-        {
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
+    if (list1 != nullptr) {
+        temp->next = list1;
+    } else {
+        temp->next = list2;
     }
+    return dummyNode->next;
+}
+ListNode* findMiddle(ListNode* head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    }
+    ListNode* slow=head;
+    ListNode* fast=head->next;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
     ListNode* sortList(ListNode* head) {
         if(head==NULL || head->next==NULL)
+        {
             return head;
-        ListNode* middle=findMiddle(head);
-        ListNode* left=head;
+        }
+        ListNode* middle =findMiddle(head);
         ListNode* right=middle->next;
         middle->next=NULL;
-        ListNode* lefthead=sortList(left);
-        ListNode* righthead=sortList(right);
-        return merge(lefthead,righthead);
+        ListNode* left=head;
+        left=sortList(left);
+        right=sortList(right);
+        return merge(left,right);
     }
 };
